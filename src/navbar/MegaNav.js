0c9528923de8399573dbd8532f18c00fe947487d
sandhyaGurram <gurramsandhya2013@gmail.com>
2025-07-23 18:19:02 +0530

    changed

---
+++ src/navbar/MegaNav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MegaNav = () => {
+    return (
+        <>
+            <div className='megamenu-1'>
+                <ul className='ul-level-1'>
+                    <li className='li-level-1'></li>
+                </ul>
+            </div>
+
+        </>
+
+    )
+}
+
+export default MegaNav
