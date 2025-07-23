80ac26fbeca0cbfe8e84e977a8c805f2cc5b6efd
sandhyaGurram <gurramsandhya2013@gmail.com>
2025-07-23 16:22:52 +0530

    changes

---
+++ src/navbar/MegaMenu.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import img1 from '../assets/img1.jpg'
+
+const MegaMenu = () => {
+    return (
+        <div className='megamenu'>
+            <ul className='level1'>
+                <li className="has-submenu">Home
+                    <ul className='level2'>
+                        <li>
+                            <img src={img1} alt='Sample 1' />
+                            <p>test level</p>
+                        </li>
+                        <li><img src={img1} alt='Sample 1' />
+                            <p>test level</p></li>
+                        <li><img src={img1} alt='Sample 1' />
+                            <p>test level</p></li>
+                        <li><img src={img1} alt='Sample 1' />
+                            <p>test level</p></li>
+                    </ul>
+                </li>
+                <li className='submenu-2'>About
+                    <ul className='sublevel2'>
+                        <li className='has-submenu2-1'>level1
+                            <ul className='level3-1'>
+                                <li><p>test level</p> </li>
+                                <li><p>test level</p></li>
+                                <li><p>test level</p></li>
+                                <li><p>test level</p></li>
+                            </ul>
+                        </li>
+                        <li className='has-submenu2-2'>level1
+                            <ul className='level3-2'>
+                                <li><p>test level-2</p> </li>
+                                <li><p>test level-2</p></li>
+                                <li><p>test level-2</p></li>
+                                <li><p>test level-2</p></li>
+                            </ul>
+                        </li>
+                        <li className='has-submenu2-3'>level1
+                            <ul className='level3-3'>
+                                <li><p>test level-3</p> </li>
+                                <li><p>test level-3</p></li>
+                                <li><p>test level-3</p></li>
+                                <li><p>test level-3</p></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li>Contact</li>
+                <li>Blogs</li>
+                <li>Channels</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MegaMenu
